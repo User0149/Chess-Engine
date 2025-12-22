7dc74da55de7d932a009172cd3b90a130fb98aed
User0149 <floatingprecision@gmail.com>
2025-12-22 23:51:02 +1100

    considers mobility when evaluating positions

--- src/engine/strategies.cpp
+++ src/engine/strategies.cpp
@@ -10,6 +10,8 @@ using namespace emscripten;
 
 // evaluates how much advantage the player to move has
 double eval(const GameState& game_state, const int depth) {
+    const double MOBILITY_FACTOR = 0.01;
+
     if (is_checkmate(game_state)) {
         return -INFINITY;
     }
@@ -20,13 +22,22 @@ double eval(const GameState& game_state, const int depth) {
     if (depth <= 0) { // base case: simply count material advantage on the board
         return game_state.eval();
     }
-
+    
     std::vector<PossibleMove> next_moves = possible_moves(game_state);
 
     double advantage = -INFINITY;
     for (PossibleMove move:next_moves) {
         advantage = std::max(advantage, -eval(move.game_state, depth - 1));
     }
+
+    // consider mobility of current state
+    GameState opp_game_state = game_state;
+    opp_game_state.to_move = (game_state.to_move == "white" ? "black" : "white");
+
+    int player_moves = (int)next_moves.size();
+    int opponent_moves = (int)possible_moves(opp_game_state).size();
+
+    advantage += MOBILITY_FACTOR * (player_moves - opponent_moves);
 
     return advantage;
 }
